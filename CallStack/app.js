e9a9ec67748dad7cd84995b0ab64acc4c7039d11
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-01-08 11:03:04 -0300

    set-time-out-learning

--- CallStack/app.js
+++ CallStack/app.js
@@ -1,11 +1,17 @@
-const multiply = (x, y) => x * y;
+// const multiply = (x, y) => x * y;
 
-const square = x => multiply(x, x);
+// const square = x => multiply(x, x);
 
-const isRigthTriangle = (a, b, c) => (
-    square(a) + square(b) === square(c)
-)
-console.log("Before!")
-isRigthTriangle(3,4,5);
+// const isRigthTriangle = (a, b, c) => (
+//     square(a) + square(b) === square(c)
+// )
+// console.log("Before!")
+// isRigthTriangle(3,4,5);
 
-console.log("Done!")
+// console.log("Done!")
+
+console.log('Sending request to server')
+setTimeout(()=> {
+    console.log('Here is your data from the server...')
+}, 3000)
+console.log("I'm at the end of the file!")
