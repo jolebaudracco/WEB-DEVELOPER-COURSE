3fc4181f89ebdc571e94d6a0e78db3d0de579c09
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-01-21 14:53:14 -0300

    modification in the fetch section

--- LearningAboutFetch/app.js
+++ LearningAboutFetch/app.js
@@ -1,14 +1,24 @@
-fetch("https://www.swapi.tech/api/people/1/")
-.then((res) => {
-    console.log("Resolved!", res);
+// fetch("https://www.swapi.tech/api/people/1/")
+// .then((res) => {
+//     console.log("Resolved!", res);
 //     return res.json();
-})
-// .then((data) => {
-//     console.log(data.result.properties);
 // })
-.catch((e) => {
-    console.log("Error!", e);
-});
+// .then((data) => {
+//     // console.log(data.result.properties);
+//     console.log(data)
+//     return fetch("https://www.swapi.tech/api/people/2/")
+// })
+// .then(res => {
+//     console.log("Second Request Resolved")
+//     return res.json();
+// })
+// .then((data) => {
+//     // console.log(data.result.properties);
+//     console.log(data)
+// })
+// .catch((e) => {
+//     console.log("Error!", e);
+// });
 
 // fetch("https://www.swapi.tech/api/people/1/")
 // .then((res) => {
@@ -30,6 +40,21 @@ fetch("https://www.swapi.tech/api/people/1/")
 //     console.log("Error", e);
 // });
 
+const loadStarWarsPeople = async () => {
+try{
+    const res = await fetch("https://www.swapi.tech/api/people/1/")
+    const data = await res.json();
+    console.log(data);
+    const res2 = await fetch("https://www.swapi.tech/api/people/2/")
+    const data2 = await res2.json();
+    console.log(data2);
+} catch(e) {
+    console.log("Error", e);
+}
+};
+
+loadStarWarsPeople();
+
 // const loadStarWarsPeople = async () => {
 //     try {
 //         const res = await fetch("https://www.swapi.tech/api/people/1/");
@@ -37,7 +62,7 @@ fetch("https://www.swapi.tech/api/people/1/")
 //         console.log(data.result.properties);
 //         const res2 = await fetch("https://www.swapi.tech/api/people/2/");
 //         const data2 = await res2.json();
-//         console.log(data.result.properties);
+//         console.log(data2.result.properties);
 //     } catch(e) {
 //         console.log("Error", e);
 //     }
@@ -45,25 +70,6 @@ fetch("https://www.swapi.tech/api/people/1/")
 
 // loadStarWarsPeople();
 
-// axios
-// .get("https://www.swapi.tech/api/people/1/")
-// .then((res) => {
-//     console.log("Response", res);
-// })
-// .catch((e) => {
-//     console.log("Error", e);
-// });
 
-// const getStarWarsPerson = async (id) => {
-//     try{
-//         const res = await axios.get(`https://swapi.tech/api/people/${id}`);
-//         console.log(res.data.result.properties);
-//     } catch (e) {
-//         console.log("Error!", e);
-//     }
-// };
-
-// getStarWarsPerson(5);
-// getStarWarsPerson(10);
 
 
