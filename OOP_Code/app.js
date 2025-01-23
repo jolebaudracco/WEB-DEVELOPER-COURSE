e197229eb88d050cf99c9ce86cc71fa935a7721b
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-01-23 11:17:04 -0300

    Prototypes presentation

---
+++ OOP_Code/app.js
@@ -0,0 +1,7 @@
+String.prototype.yell = function() {
+    return `OMG!!! ${this.toUpperCase()}!!!! NAAAAHHH!!!`;
+};
+
+Array.prototype.pop = function() {
+    return 'Sorry, I want that element, I will never pop it off';
+};
