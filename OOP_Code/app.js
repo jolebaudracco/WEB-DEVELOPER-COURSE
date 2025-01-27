792fd856c9dcb02075f3d2052cc230d479a15b5b
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-01-27 15:14:50 -0300

    changes in classes training

--- OOP_Code/app.js
+++ OOP_Code/app.js
@@ -50,35 +50,70 @@
 // black.rgb(); //"rgb(0,0,0)"
 // black.hex(); //"#0000s00"
 
-// Create a blank, plain Javascript Object;
-// Links (sets the constructor of) this object to another object;
-// Passes the newly created object from Step 1 as the this context;
-// Returns this if the function doesn't return its own object.
 
-function Color(r,g,b){
-    this.r = r;
-    this.g = g;
-    this.b = b;
-};
+//This is a constructor function
+// function Color(r,g,b){
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+// };
 
-Color.prototype.rgb = function() {
-    const { r, g, b } = this;
-    return `rgb(${r}, ${g}, ${b})`; 
-};
+//If you call it on its own like a regular function... 
+// Color(35, 60, 190)
+//it returns undefined. Seems useless!
 
-Color.prototype.hex = function() {
-    const { r, g, b } = this;
-    return '#' + ((1 << 24) + (r << 18) + (g << 8) + b).toString(16).slice(1);  
-};
+//*****************
+//THE NEW OPERATOR
+//*****************
 
-Color.prototype.rgba = function(a=1.0){
-    const { r, g, b } = this;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-document.body.style.backgroundColor = color1.rgb()
+// 1. Create a blank, plain Javascript Object;
+// 2. Links (sets the constructor of) this object to another object;
+// 3. Passes the newly created object from Step 1 as the this context;
+// 4. Returns this if the function doesn't return its own object.
+
+// Color.prototype.rgb = function() {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`; 
+// };
+
+// Color.prototype.hex = function() {
+//     const { r, g, b } = this;
+//     return '#' + ((1 << 24) + (r << 18) + (g << 8) + b).toString(16).slice(1);  
+// };
+
+// Color.prototype.rgba = function(a=1.0){
+//     const { r, g, b } = this;
+//     return `rgba(${r}, ${g}, ${b}, ${a})`;
+// }
+
+// const color1 = new Color(40, 255, 60);
+// color1.hex();
+// const color2 = new Color(0, 0, 0);
+// color2.hex();
+
+//Classes in JS
+class Color {
+    constructor(r, g, b, name){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    innerRGB(){
+        const { r, g, b } = this;
+        return `${r}, ${g}, ${b}`;
+    }
+    rgb(){
+        return `rgb(${this.innerRGB()})`; 
+    }
+    rgba(a=1.0){
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hex(){
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 18) + (g << 8) + b).toString(16).slice(1);
+    }
 }
-
-const color1 = new Color(40, 255, 60);
-color1.hex();
-const color2 = new Color(0, 0, 0);
-color2.hex();
+const red = new Color(255,67,89, 'tomato');
+const white = new Color(255,255,255, 'white');
 
